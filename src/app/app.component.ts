f9a5759693b4d044bc6035a26da1329826fc8e6b
Daniel <danielzanoni2001@gmail.com>
2022-10-10 18:16:15 -0300

    Setado o Titulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'requisicoes-app';
+  title = 'Requisições';
+
+  constructor(titulo: Title){
+    titulo.setTitle("Início - RequisiçõesApp");
+  }
 }
