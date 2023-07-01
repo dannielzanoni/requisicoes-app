07802d34a1a41abb34d60240d5a640d49c036cac
DanielZanoniSantos <danielzanoni2001@gmail.com>
2023-07-01 02:27:11 -0300

    added validations on login screen

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -12,14 +12,15 @@ import { AuthenticationService } from '../services/authentication.service';
 export class LoginComponent implements OnInit {
   public form: FormGroup;
   public formRecuperacao: FormGroup;
+  public loading: boolean = false;
 
   constructor
-  (
-    private formBuilder: FormBuilder,
-    private authService: AuthenticationService,
-    private router: Router,
-    private modalService: NgbModal
-    ) {}
+    (
+      private formBuilder: FormBuilder,
+      private authService: AuthenticationService,
+      private router: Router,
+      private modalService: NgbModal
+    ) { }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
@@ -33,43 +34,71 @@ export class LoginComponent implements OnInit {
 
   }
 
-  get email(): AbstractControl | null{
+  get email(): AbstractControl | null {
     return this.form.get("email");
   }
 
-  get senha(): AbstractControl | null{
+  get senha(): AbstractControl | null {
     return this.form.get("senha");
   }
 
-  get emailRecuperacao(): AbstractControl | null{
+  get emailRecuperacao(): AbstractControl | null {
     return this.formRecuperacao.get("emailRecuperacao");
   }
 
-  public async login(){
+  public abrirModalRecuperacao(modal: TemplateRef<any>) {
+    this.modalService.open(modal)
+      .result
+      .then(resultado => {
+        if (resultado === "enviar") {
+          this.authService.resetarSenha(this.emailRecuperacao?.value);
+        }
+      })
+      .catch(() => {
+        this.formRecuperacao.reset();
+      });
+  }
+  
+  public async login() {
     const email = this.email?.value;
     const senha = this.senha?.value;
 
-    try{
-      const resposta = await this.authService.login(email, senha);
+    if(!email || !senha){
+      var popupEmail = document.getElementById('popup-email');
+      popupEmail!.classList.remove('hidden');
 
-      if(resposta?.user){
+      const currentWith = popupEmail?.offsetWidth;
+      const newWith = currentWith! + 20;
+
+      popupEmail!.style.width = newWith! + 'px';
+    }
+
+    this.loading = true;
+    try {
+      const resposta = await this.authService.login(email, senha);
+      if (resposta?.user) {
         this.router.navigate(["/painel"]);
       }
-    } catch(error){
+      
+    } catch (error) {
+      this.loading = false;
       console.log(error);
+      if(email && senha){
+        var popup = document.getElementById('popup');
+        popup!.classList.remove('hidden');
+        
+        const currentWith = popup?.offsetWidth;
+        const newWith = currentWith! + 20;
+
+        popup!.style.width = newWith! + 'px';
+      }
     }
   }
 
-  public abrirModalRecuperacao(modal: TemplateRef<any>){
-    this.modalService.open(modal)
-    .result
-    .then(resultado => {
-      if(resultado === "enviar"){
-        this.authService.resetarSenha(this.emailRecuperacao?.value);
-      }
-    })
-    .catch(() =>{
-      this.formRecuperacao.reset();
-    });
+  resetPopup() {
+    var popup = document.getElementById('popup');
+    var popupEmail = document.getElementById('popup-email');
+    popup!.classList.add('hidden');
+    popupEmail!.classList.add('hidden');
   }
 }
